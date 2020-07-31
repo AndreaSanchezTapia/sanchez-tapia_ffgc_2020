74755fb10a5e987b28d36d8d8014843bac3056bc
Andrea Sánchez Tapia <katori@gmail.com>
2020-07-30 18:49:54 -0700

    maps

--- scripts/1_mapa.R
+++ scripts/1_mapa.R
@@ -1,90 +1,183 @@
 library(rgdal)
 library(scales)
-library(maps)
-#palette change in july 2020
+library(tmap)
+library(sf)
+library(spData)
+library(dplyr)
 library(RColorBrewer)
+library(grid)
+
+#palette change in july 2020
 pal <- brewer.pal(5, "PiYG")
 
+
+dir <- "./data/1_map/baf"
+shp <- list.files(dir, full.names = T, pattern = ".shp$")
+
+# baf and s. america shapes
+bioma <- read_sf(shp[1])
+sam <- read_sf(shp[4])
+
+# prepares colour column and annotation for the inset
+bioma <- bioma %>%
+  mutate(col = case_when(
+    LEGEND == "ATLANTIC FOREST" ~  "darkseagreen4",
+    LEGEND == "CERRADO" ~  "grey75",
+    !LEGEND %in% c("ATLANTIC FOREST", "CERRADO") ~ "grey75"))
+#This is only for label positioning for the inset:
+bioma <- bioma %>%
+  mutate(lab = if_else(LEGEND == "CERRADO", "BAF", ""))
+
+
 #reads official shapefiles
-PDA     <- readOGR("./data/1_map/shapefiles/rbpda/", "LimitedaRB", verbose = F)
-br101   <- readOGR("./data/1_map/shapefiles/rbpda/", "BR_101", verbose = F)
-saojoao <- readOGR("./data/1_map/shapefiles/rbpda/", "riosaojoao", verbose = F)
-SOSma   <- readOGR(dsn = "./data/1_map/shapefiles/brasil/SOSMA/",
-                   layer = "rema_fd4d801731725513a4d77aa9bb35534b5305",
-                   verbose = F)
+PDA     <- read_sf("./data/1_map/shapefiles/rbpda/", "LimitedaRB")
+br101   <- read_sf("./data/1_map/shapefiles/rbpda/", "BR_101")
+saojoao <- read_sf("./data/1_map/shapefiles/rbpda/", "riosaojoao")
+SOSma   <- read_sf("./data/1_map/shapefiles/brasil/SOSMA/rema_fd4d801731725513a4d77aa9bb35534b5305.shp")
+
 #assigns the correct CRS (UTM in Brazil SAD69)
-proj4string(PDA)     <- CRS("+init=epsg:29193")
-proj4string(br101)   <- CRS("+init=epsg:29193")
-proj4string(saojoao) <- CRS("+init=epsg:29193")
+
+PDA     <- st_set_crs(PDA, 29193)
+br101   <- st_set_crs(br101, 29193)
+saojoao   <- st_set_crs(saojoao, 29193)
 
 # reprojects to WGS84
-PDA.wsg84     <- spTransform(PDA, CRS("+proj=longlat +datum=WGS84"))
-SOS.wgs84     <- spTransform(SOSma, CRS("+proj=longlat +datum=WGS84"))
-br101.wgs84   <- spTransform(br101, CRS("+proj=longlat +datum=WGS84"))
-saojoao.wgs84 <- spTransform(saojoao, CRS("+proj=longlat +datum=WGS84"))
+PDA.wsg84     <- st_transform(PDA, 4326)
+SOS.wgs84     <- st_transform(SOSma, 4326)
+br101.wgs84   <- st_transform(br101, 4326)
+saojoao.wgs84 <- st_transform(saojoao, 4326)
 
 # reads the shapefiles of the fires - digitalized by AST from paper arquives from the PABR
 #1993 fire
-fuego1993 <- readOGR(dsn = "./data/1_map/queimadas", layer = "incendio1993" , verbose = F)
-proj4string(fuego1993) <- CRS("+init=epsg:29193")## isto é UTM em SAD69 brasileiro
-fuego1993.wsg84 <- spTransform(fuego1993, CRS("+proj=longlat +datum=WGS84"))
+fuego1993 <- read_sf("./data/1_map/queimadas/incendio1993.shp")
+fuego1993 <- st_set_crs(fuego1993, 29193)## isto é UTM em SAD69 brasileiro
+fuego1993.wsg84 <- st_transform(fuego1993, 4326)
 
 #2002 fire
-fuego2002 <- readOGR(dsn = "./data/1_map/queimadas", layer = "incendio2002", verbose = F)
-fuego2002.wsg84 <- spTransform(fuego2002, CRS("+proj=longlat +datum=WGS84"))
+fuego2002 <- read_sf("./data/1_map/queimadas/incendio2002.shp")
+st_crs(fuego2002)
+fuego2002.wsg84 <- st_transform(fuego2002, 4326)
 
 #2007 fire
-fuego2007 <- readOGR(dsn = "./data/1_map/queimadas", layer = "Incendio_2007", verbose = F)
-fuego2007.wsg84 <- spTransform(fuego2007, CRS("+proj=longlat +datum=WGS84"))
+fuego2007 <- read_sf("./data/1_map/queimadas/Incendio_2007.shp")
+st_crs(fuego2007)
+fuego2007.wsg84 <- st_transform(fuego2007, 4326)
 
 #2010 fire
-fuego2010 <- readOGR(dsn = "./data/1_map/queimadas", layer = "incendio2010p", verbose = F)
-fuego2010.wsg84 <- spTransform(fuego2010, CRS("+proj=longlat +datum=WGS84"))
+fuego2010 <- read_sf("./data/1_map/queimadas/incendio2010p.shp")
+st_crs(fuego2010)
+fuego2010.wsg84 <- st_transform(fuego2010, 4326)
 
 # reads plot shapefiles
 #2002, 2010
-parcelas <- readOGR(dsn = "./data/1_map/shapefiles/", layer = "parcelasandre", verbose = F)
-parcelas.wsg84 <- spTransform(parcelas, CRS("+proj=longlat +datum=WGS84"))
+parcelas <- read_sf("./data/1_map/shapefiles/parcelasandre.shp")
+st_crs(parcelas)
+parcelas.wsg84 <- st_transform(parcelas, 4326)
+
 # 1990, secondary forest, mature forest
-parcelas_j <- readOGR(dsn = "./data/1_map/shapefiles/", layer = "parcelasjeronimo", verbose = F)
-parcelas_j_wsg84 <- spTransform(parcelas_j, CRS("+proj=longlat +datum=WGS84"))
+parcelas_j <- read_sf("./data/1_map/shapefiles/parcelasjeronimo.shp")
+st_crs(parcelas_j)
+parcelas_j_wsg84 <- st_transform(parcelas_j, 4326)
+parcelas.wsg84 <- parcelas.wsg84 %>% mutate(col = rep(c(pal[1], pal[2]), each = 3))
+parcelas_j_wsg84 <- parcelas_j_wsg84 %>%
+  slice(1:9) %>%
+  mutate(col = rep(c(pal[3:5]), each = 3))
 
 #creates the map and saves it to png file
-png("./figs/1_map.png", res = 300, height = 500*300/72, width = 500*300/72)
-par(mfrow = c(1,1), mar = c( 3, 3, 1.5, 1))
-plot(PDA.wsg84, add = F, border = "black")
-plot(SOS.wgs84, add = T, col = alpha("lightgreen", 0.3), border = alpha("darkgreen", 0.1))
-plot(fuego1993.wsg84, add = T, border = "grey90", col = alpha("grey", 0.4))
-plot(fuego2002.wsg84, add = T, border = "grey90", col = alpha("grey", 0.4))
-plot(fuego2007.wsg84, add = T, border = "grey90", col = alpha("grey", 0.4))
-plot(fuego2010.wsg84, add = T, border = "grey90", col = alpha("grey", 0.4))
-plot(saojoao.wgs84,   add = T, border = "dodgerblue", lwd = 2)
-plot(br101.wgs84,     add = T, col = "red", lwd = 2)
-plot(PDA.wsg84,       add = T, border = "black", lwd = 1.5)
-map.scale(ratio = F, cex = 1.4)
-map.axes(cex.axis = 1.4)
-#plot points
-points(parcelas.wsg84, bg = rep(c(pal[1], pal[2]), each = 3), pch = 21, cex = rep(c(1.5, 1.5), each = 3), col = "black")
-points(parcelas_j_wsg84[1:9, ], bg = rep(c(pal[3:5]), each = 3), pch = 21, cex = rep(c(1.5, 1.5, 1.5), each = 3), col = "black")
-#legend
-legend(x = -42.26, y = -22.5,
-       fill = c(alpha("lightgreen", 0.7), alpha("grey", 0.8)),
-       legend = c("Forest remnants", "Fires 1993-2010"),
-       box.lwd = "n",
-       cex = 1,
-       border = "grey80")
-legend(x = -42.26, y = -22.51,
-       pch = 21,
-       legend = c("High-disturbance",
+pabr <-
+  tm_shape(PDA.wsg84) +
+  tm_borders(lwd = 1.5) +
+  tm_shape(SOS.wgs84) +
+  tm_fill(col = "lightgreen", alpha = 0.3) +
+  tm_borders(col = "darkgreen", alpha = 0.1) +
+  tm_shape(fuego1993.wsg84) +
+  tm_fill(col = "grey", alpha = 0.4) +
+  tm_borders(col = "grey90") +
+  tm_shape(fuego2002.wsg84) +
+  tm_fill(col = "grey", alpha = 0.4) +
+  tm_borders(col = "grey90") +
+  tm_shape(fuego2007.wsg84) +
+  tm_lines(col = "grey", alpha = 0.4) +
+  #tm_borders(col = "grey90") +
+  tm_shape(fuego2010.wsg84) +
+  tm_fill(col = "grey", alpha = 0.4) +
+  tm_borders(col = "grey90") +
+  tm_shape(saojoao.wgs84) +
+  tm_borders(col = "dodgerblue", lwd = 1.5) +
+  tm_shape(br101.wgs84) +
+  tm_lines(col = "red", lwd = 2) +
+  tm_shape(PDA.wsg84) +
+  tm_borders(col = "black", lwd = 1.5) +
+  tm_shape(parcelas.wsg84) +
+  tm_symbols(col = "col", size = 0.5, border.col = "black") +
+  tm_shape(parcelas_j_wsg84) +
+  tm_symbols(col = "col", size = 0.5, border.col = "black") +
+  tm_add_legend(labels = c("Fires 1993 - 2010", "BAF fragments"),
+                col = c("grey","lightgreen"),
+                alpha = c(0.4),
+                type = "fill",
+                size = 3
+  ) +
+  tm_add_legend(labels = c("PABR limits","Roads"),
+                col = c("black","red"),
+                type = "line",
+                lwd = 1.5) +
+  tm_compass(type = "arrow", position = c("left", "bottom")) +
+  tm_scale_bar(breaks = c(0, 1, 2),
+               text.size = 1,
+               position = c("left", "bottom")) +
+  tm_layout(frame = F,
+            legend.outside = F,
+            legend.position  = c("right", "top"),
+            legend.height = 1.5,
+            legend.width = 1,
+            legend.just = 1) +
+  tm_add_legend(type = "symbol",
+                labels = c(
+                  "High disturbance",
                   "Medium disturbance",
                   "Low disturbance",
-                  "Secondary forests",
-                  "Mature forests"),
-       box.lwd = "n",
-       pt.bg = pal,
-       col = "black",
-       pt.cex = c(1.5, 1.5, 1.5, 1.5, 1.5), cex = 1)
-#mtext("C", 3, las = 1, line = 0.2, cex = 1.5, adj = 0)
-dev.off()
+                  "Sec. Forest",
+                  "Mature Forest"
+                ),
+                #size = 0.5,
+                #legend.format = 1.5,
+                col = pal) +
+  NULL
+pabr
+#tmap_save(tm = pabr, filename = "./figs/1_map_pabr.png")
 
+pabr_region <- st_bbox(PDA.wsg84) %>%
+  st_as_sfc()
+
+#mapa inset----
+library(brazilmaps)
+br <- brazilmaps::get_brmap("Brazil")
+study_area <- sam %>% filter(is.na(NM_ESTADO)) %>%
+  tm_shape(., bbox = st_bbox(point)) +
+  tm_borders() +
+  tm_shape(br) +
+  tm_borders() +
+  tm_shape(bioma) +
+  tm_fill(col = "col", alpha = 0.7) +
+  tm_text(text = "lab", size = 2, col = "black") +
+  tm_shape(SOS.wgs84) +
+  tm_borders(col = "red") +
+  #tm_add_legend(type = "fill",
+  #             col = c("chartreuse4"),
+  #            labels = c("Brazilian Atlantic Forest (BAF)")) +
+  tm_add_legend(labels = "PABR",
+                type = "symbol",
+                col = "red",
+                shape = 19,
+                size = 1.5)
+study_area
+tmap_save(study_area, "./figs/00_study_area.png", width = 5, height = 6)
+
+
+png(filename = "./figs/1_map.png", res = 300,
+    width = 300 / 72 * 500, height = 300 / 72 * 500)
+pabr
+print(study_area, vp = viewport(0.15, 0.85, width = 0.25, height = 0.25))
+dev.off()
 
--- scripts/7_map.R
+++ scripts/7_map.R
@@ -1,199 +1,180 @@
+# load packages
 library(rgdal)
 library(scales)
-library(tmaps)
+library(tmap)
 library(sf)
 library(spData)
-#palette change in july 2020
+library(dplyr)
+library(grid)
 library(RColorBrewer)
+library(grDevices)
+library(brazilmaps)
+
+# brazil shapefile
+br <- brazilmaps::get_brmap("Brazil")
+
+#palette change in july 2020
 pal <- brewer.pal(5, "PiYG")
 
-#reads official shapefiles
-PDA     <- read_sf("./data/1_map/shapefiles/rbpda/", "LimitedaRB")
-br101   <- read_sf("./data/1_map/shapefiles/rbpda/", "BR_101")
-saojoao <- read_sf("./data/1_map/shapefiles/rbpda/", "riosaojoao")
-SOSma   <- read_sf("./data/1_map/shapefiles/brasil/SOSMA/rema_fd4d801731725513a4d77aa9bb35534b5305.shp")
+# makes the maps of the whole species distribution----
 
-#assigns the correct CRS (UTM in Brazil SAD69)
+dir <- "./data/1_map/baf"
+shp <- list.files(dir, full.names = T, pattern = ".shp$")
 
-PDA     <- st_set_crs(PDA, 29193)
-br101   <- st_set_crs(br101, 29193)
-saojoao   <- st_set_crs(saojoao, 29193)
+bioma <- read_sf(shp[1])
+geo_dist <- read_sf(shp[2])
+point <- read_sf(shp[3])
+sam <- read_sf(shp[4])
 
-# reprojects to WGS84
-PDA.wsg84     <- st_transform(PDA, 4326)
-SOS.wgs84     <- st_transform(SOSma, 4326)
-br101.wgs84   <- st_transform(br101, 4326)
-saojoao.wgs84 <- st_transform(saojoao, 4326)
+# prepares colour column and annotation for the inset
+bioma <- bioma %>% mutate(col = case_when(
+  LEGEND == "ATLANTIC FOREST" ~  "darkseagreen4",
+  LEGEND == "CERRADO" ~  "grey75",
+  !LEGEND %in% c("ATLANTIC FOREST", "CERRADO") ~ "grey75"))
+bioma <- bioma %>%
+  mutate(lab = if_else(LEGEND == "CERRADO", "BAF", "")) #This is only for label positioning
 
-# reads the shapefiles of the fires - digitalized by AST from paper arquives from the PABR
-#1993 fire
-fuego1993 <- read_sf("./data/1_map/queimadas/incendio1993.shp")
-fuego1993 <- st_set_crs(fuego1993, 29193)## isto é UTM em SAD69 brasileiro
-fuego1993.wsg84 <- st_transform(fuego1993, 4326)
+# sets color for the whole set of points and
+point <- point %>%
+  mutate(col = case_when(
+  Mono =="Occurrence" ~ "white",
+  Mono =="No" ~ "black",
+  Mono =="Yes" ~ "red"))
 
-#2002 fire
-fuego2002 <- read_sf("./data/1_map/queimadas/incendio2002.shp")
-st_crs(fuego2002)
-fuego2002.wsg84 <- st_transform(fuego2002, 4326)
+# ...for the points that were actually visited----
+point_det <- point %>%
+  filter(Mono != "Occurrence") %>%
+  mutate(col = case_when(
+    Mono =="No" ~ "black",
+    Mono =="Yes" ~ "red"))
 
-#2007 fire
-fuego2007 <- read_sf("./data/1_map/queimadas/Incendio_2007.shp")
-st_crs(fuego2007)
-fuego2007.wsg84 <- st_transform(fuego2007, 4326)
-
-#2010 fire
-fuego2010 <- read_sf("./data/1_map/queimadas/incendio2010p.shp")
-st_crs(fuego2010)
-fuego2010.wsg84 <- st_transform(fuego2010, 4326)
-
-# reads plot shapefiles
-#2002, 2010
-parcelas <- read_sf("./data/1_map/shapefiles/parcelasandre.shp")
-st_crs(parcelas)
-parcelas.wsg84 <- st_transform(parcelas, 4326)
-
-# 1990, secondary forest, mature forest
-parcelas_j <- read_sf("./data/1_map/shapefiles/parcelasjeronimo.shp")
-st_crs(parcelas_j)
-parcelas_j_wsg84 <- st_transform(parcelas_j, 4326)
-parcelas.wsg84 <- parcelas.wsg84 %>% mutate(col = rep(c(pal[1], pal[2]), each = 3))
-parcelas_j_wsg84 <- parcelas_j_wsg84 %>% slice(1:9) %>% mutate(col = rep(c(pal[3:5]), each = 3))
-
-#creates the map and saves it to png file
-library(tmap)
-pabr <- tm_shape(PDA.wsg84) +
-  tm_borders(lwd = 1.5) +
-tm_shape(SOS.wgs84) +
-  tm_fill(col = "lightgreen", alpha = 0.3) +
-  tm_borders(col = "darkgreen", alpha = 0.1) +
-tm_shape(fuego1993.wsg84) +
-  tm_fill(col = "grey", alpha = 0.4) +
-  tm_borders(col = "grey90") +
-tm_shape(fuego2002.wsg84) +
-  tm_fill(col = "grey", alpha = 0.4) +
-  tm_borders(col = "grey90") +
-tm_shape(fuego2007.wsg84) +
-  tm_lines(col = "grey", alpha = 0.4) +
-  #tm_borders(col = "grey90") +
-tm_shape(fuego2010.wsg84) +
-  tm_fill(col = "grey", alpha = 0.4) +
-  tm_borders(col = "grey90") +
-tm_shape(saojoao.wgs84) +
-  tm_borders(col = "dodgerblue", lwd = 1.5) +
-tm_shape(br101.wgs84) +
-    tm_lines(col = "red", lwd = 2) +
-tm_shape(PDA.wsg84) +
-    tm_borders(col = "black", lwd = 1.5) +
-tm_shape(parcelas.wsg84) +
-  tm_symbols(col = "col", size = 1, border.col = "black") +
-tm_shape(parcelas_j_wsg84) +
-  tm_symbols(col = "col", size = 1, border.col = "black") +
-  tm_add_legend(labels = c("Fires 1993 - 2010", "BAF fragments"),
-                col = c("grey","lightgreen"),
-                alpha = c(0.4),
-                type = "fill",
-                size = 3
-                ) +
-  tm_add_legend(labels = c("PABR limits","Roads"),
-                col = c("black","red"),
-                type = "line",
-                lwd = 1.5,
-                ) +
-  tm_compass(type = "arrow", position = c("left", "bottom")) +
-  tm_scale_bar(breaks = c(0, 1, 2), text.size = 1, position = c("left", "bottom")) +
-  tm_layout(frame = F,
-            legend.outside = F,
-            legend.position  = c("right", "top"),
-            legend.height = 1.5,
-            legend.width = 1,
-            legend.just = 1,
-            ) +
-  tm_add_legend(type = "symbol",
-                labels = c("Mature Forest",
-                           "Sec. Forest",
-                           "Low disturbance",
-                           "Medium disturbance",
-                           "High disturbance"
-                           ),
-                col = pal) +
-  NULL
-pabr
-st_bbox(point)
-pabr_region <- st_bbox(PDA.wsg84) %>%
+inset_region <- st_bbox(point_det) %>%
   st_as_sfc()
 
-#plot points
-#legend
-legend(x = -42.26, y = -22.5,
-       fill = c(alpha("lightgreen", 0.7), alpha("grey", 0.8)),
-       legend = c("Forest remnants", "Fires 1993-2010"),
-       box.lwd = "n",
-       cex = 1,
-       border = "grey80")
-legend(x = -42.26, y = -22.51,
-       pch = 21,
-       legend = c("High-disturbance",
-                  "Medium disturbance",
-                  "Low disturbance",
-                  "Secondary forests",
-                  "Mature forests"),
-       box.lwd = "n",
-       pt.bg = pal,
-       col = "black",
-       pt.cex = c(1.5, 1.5, 1.5, 1.5, 1.5), cex = 1)
-#mtext("C", 3, las = 1, line = 0.2, cex = 1.5, adj = 0)
+bioma_map <- tm_shape(bioma, bbox = st_bbox(point)) +
+  tm_fill("col", alpha = 1) +
+  tm_shape(point) +
+  tm_symbols(col = "col", size = 0.15, alpha = 0.8) +
+  tm_shape(inset_region) +
+  tm_borders(lwd = 1.5) +
+  tm_add_legend(type = "fill",
+                col = "darkseagreen4",
+                alpha = 1,
+                labels = c("BAF")) +
+  tm_add_legend(type = "symbol",
+                labels = c("Known occurrences",
+                           "Confirmed monodominance (>60%)",
+                           "Confirmed non-monodominance (<60%)"),
+                col = c("white", "red", "black")) +
+  tm_layout(legend.width = 2) +
+  tm_shape(br) +
+  tm_borders() +
+#  tm_layout(title = expression(paste(italic("M. polymorphum"),
+ #                                    " in the Brazilian Atlantic Forest")),
+  #          title.fontface = 2,
+   #         title.bg.alpha = 1,
+    #        title.size = 1.5) +
+  NULL
+bioma_map
+
+tmap_save(tm = bioma_map, filename = "./figs/7_map_bioma3.png")
+
+#detailed inset
+
+
+bioma_det <- tm_shape(bioma, bbox = st_bbox(inset_region)) +
+  tm_fill("col", alpha = 1) +
+  tm_shape(point_det) +
+  tm_symbols(col = "col", size = 0.3, alpha = 0.7) +
+#  tm_add_legend(type = "fill",
+ #               col = "darkseagreen4",
+  #              alpha = 1,
+   #             labels = c("BAF")) +
+#  tm_add_legend(type = "symbol",
+ #               labels = c("Confirmed monodominance (>60%)",
+  #                         "Confirmed non-monodominance (<60%)"),
+   #             col = c("white", "red", "black"),
+    #            size = 0.3) +
+#  tm_layout(title = expression(paste(italic("M. polymorphum"),
+ #                                    " in the Brazilian Atlantic Forest")),
+  #          title.fontface = 2,
+   #         title.bg.alpha = 1,
+    #        title.size = 1.5) +
+  NULL
+
+bioma_det
+png(filename = "./figs/7_map_bioma4.png", res = 300,
+    width = 300 / 72 * 500, height = 300 / 72 * 500)
+bioma_map
+print(bioma_det, vp = viewport(0.15, 0.85, width = 0.3, height = 0.3))
 dev.off()
 
 
 
-library(tmap)
-library(dplyr)
-# map
 
-library(sf)
-dir <- "./data/1_map/baf/Andrea"
-pts <- "./data/1_map/baf/Andrea 2"
+# in which biomes is the species present?
+st_join(point, bioma, st_intersects) %>% count(CD_LEGENDA)
+#27 caatinga, 82 cerrado, 395 Baf, 26 pampa
 
-shp <- list.files(dir, full.names = T, pattern = ".shp$")
-pts <- list.files(pts, full.names = T, pattern = ".shp$")
-shp
-bioma <- read_sf(shp[1])
-geo_dist <- read_sf(shp[2])
-sam <- read_sf(shp[4])
-point <- read_sf(pts)
+# reads all the sosma shapefiles
+dirs <- list.files("./data/sosma", full.names = TRUE, recursive = TRUE, pattern = ".zip")
 
+#unzips
 
-bioma$col <- ifelse(bioma$LEGEND == "ATLANTIC FOREST", "darkgreen", "grey90")
-point <- point %>%
-  mutate(col = case_when(
-  Mono =="Occurrence" ~ "grey",
-  Mono =="No" ~ "black",
-  Mono =="Yes" ~ "red"))
-library(grDevices)
-bioma_map <- tm_shape(bioma, bbox = st_bbox(point)) +
-  tm_fill("col", alpha = 0.5) +
-  tm_shape(point) +
-  tm_symbols(col = "col", size = 0.3, alpha = 0.7) +
-  tm_add_legend(type = "fill",
-                col = "darkgreen",
-                alpha = 0.5,
-                labels = c("BAF")) +
-  tm_add_legend(type = "symbol",
-                labels = c("Occurrences", "Monodominance", "Non-monodominance"),
-                col = c("grey", "red", "black")) +
-  tm_layout(title = expression(paste(italic("M. polymorphum"), " in the Brazilian Atlantic Forest")),
-            title.fontface = 2,
-            title.size = 1,
-            title.bg.color = "white") +
-  NULL
+#purrr::map_df(dirs, ~unzip(zipfile = ., exdir = "./data/sosma"))
+
+sosma_all <- list.files("./data/sosma", pattern = ".shp$", full.names = TRUE)
+estados <- stringr::str_sub(basename(sosma_all), 1, 2)
+sosma_all <- purrr::map(sosma_all, ~read_sf(.))
+names(sosma_all) <- estados
+
+# only forest
+mata <- purrr::map(sosma_all, ~filter(., legenda == "Mata"))
+#only bounding boxes
+bboxes <- purrr::map(mata, ~st_bbox(.))
+bboxes <- purrr::map(bboxes, ~st_as_sfc(.))
+bbox_all <- st_union(bboxes[[1]], bboxes[[2]]) %>%
+  st_union(bboxes[[3]]) %>%
+  st_union(bboxes[[4]]) %>%
+  st_union(bboxes[[5]]) %>%
+  st_union(bboxes[[6]]) %>%
+  st_union(bboxes[[7]]) %>%
+  st_union(bboxes[[8]]) %>%
+  st_union(bboxes[[9]])
 
 
 
-tmap_save(tm = pabr, filename = "./figs/1_map_pabr.png")
-tmap_save(tm = bioma_map, filename = "./figs/7_map_bioma.png")
-tmap_arrange(bioma_map, pabr)
+###interseções só com mata----
+intersections <- purrr::map(.x = mata,
+                            ~st_join(x = point,
+                                     y = .x,
+                                     join = st_intersects))
+points_in_mata <- purrr::map(intersections, ~count(., legenda, UF))
+dominancia_in_mata <- purrr::map(intersections, ~count(., legenda, Mono, UF))
 
-library(grid)
-bioma_map
-pabr
-print(bioma_map, vp = viewport(0.1, 0.8, width = 0.25, height = 0.25))
+bind_rows(points_in_mata) %>% filter(!is.na(UF)) %>% tally(n)
+# only 98 points in forest features
+bind_rows(dominancia_in_mata) %>% filter(!is.na(UF)) %>% count(Mono)
+# 47 no, 48 occs (unvisited) 3 yes
+
+# in which features are these points
+inter_tudo <- purrr::map(.x = sosma_all,
+                            ~st_join(x = point,
+                                     y = .x,
+                                     join = st_intersects))
+inter_tudo
+points_in_baf <- purrr::map(inter_tudo, ~count(., legenda, UF))
+points_in_baf2 <- purrr::map(inter_tudo, ~count(., legenda))
+dominancia_in_baf <- purrr::map(inter_tudo, ~count(., legenda, Mono, UF))
+
+bind_rows(points_in_baf) %>% filter(!is.na(UF)) %>%
+  count(legenda)
+# 98 in forest, 41 in urbanized areas, 13 in non forest areas: 152 points
+
+bind_rows(dominancia_in_baf) %>% filter(!is.na(UF)) %>%
+  #count(legenda, Mono)
+  tally()
+
+#395 Baf  only 152 fall into some part of the shape
+inter_tudo_one <- bind_rows(inter_tudo) %>% filter(!is.na(UF))
+inter_tudo_one %>% count(Mono, legenda)
